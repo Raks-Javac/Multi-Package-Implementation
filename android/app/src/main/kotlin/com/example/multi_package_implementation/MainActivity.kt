314c945a6a64e280d0c9074e32ead9c6082bd5b4
Raks-Javac <rufaikudus2014@gmail.com>
2023-01-26 02:48:18 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/multi_package_implementation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.multi_package_implementation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ package/b/lib/b.dart
@@ -0,0 +1,12 @@
+library b;
+
+import 'package:flutter/material.dart';
+
+class HelloB extends StatelessWidget {
+  const HelloB({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
